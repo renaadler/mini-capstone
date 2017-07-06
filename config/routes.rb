685a56a1f4317862fcf6029b170c2adafb6c0b29
“Rena” <renaadler1@gmail.com>
2017-07-06 18:26:28 -0500

    Create single product page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
+
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get "/products_url" => "products#products_method"
+  get "/mango_product_url" => "products#mango_product_method"
 end
