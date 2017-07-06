12fb895480685a542a0a1365db690c1e7596a246
“Rena” <renaadler1@gmail.com>
2017-07-05 21:27:53 -0500

    Added all products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,11 @@
 class ProductsController < ApplicationController
+  def products_method
+    @products = Product.all
+    render "products_page.html.erb"
+  end
 end
+
+# +  def one_contact_method
+#  +    @contact = Contact.first
+#  +    render "one_contact.html.erb"
+#  +  end
