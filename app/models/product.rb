47952769aab1a07023808c795e296e6e721e857e
“Rena” <renaadler1@gmail.com>
2017-07-12 21:12:14 -0500

    Added new methods in model for saels tax, and display messages. Also updated some CSS

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,17 @@
 class Product < ApplicationRecord
+  def sales_method
+    if price < 6 
+      @message = "Discount Item!" 
+    else 
+      @message = "Everyday Value"
+    end
+  end
+
+  def tax
+    price * 0.09
+  end
+
+  def total
+    price + tax
+  end
 end
