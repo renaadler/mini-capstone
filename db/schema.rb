f0e2666cda21f0ac264d47d66be672fd692de987
“Rena” <renaadler1@gmail.com>
2017-07-18 19:49:39 -0500

    Changed Product to Supplier ID

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170719004242) do
+ActiveRecord::Schema.define(version: 20170719004617) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 20170719004242) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean "stock"
+    t.integer "supplier_id"
   end
 
   create_table "suppliers", force: :cascade do |t|
