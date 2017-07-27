8b339a196ebfcc3891c6640b85afe5fb6e62d32f
“Rena” <renaadler1@gmail.com>
2017-07-27 18:36:51 -0500

    deleted image from new form, added new CartedProducts model and removed product_id and quantity from Orders table

--- app/views/products/select.rb
+++
@@ -1,8 +0,0 @@
-movies = [
-  {id: 1, title: "Die Hard", rating: 4.0},
-  {id: 2, title: "Bad Boys", rating: 5.0},
-  {id: 3, title: "The Chamber", rating: 3.0},
-  {id: 4, title: "Fracture", rating: 2.0}
-]
-
-movies.select { |movie| movie rating: == 4.0}
